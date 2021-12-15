94f42fb558530bd6b1bb0a8a42366add4d5738be
Juan Miguel Requena Mullor <juanmimullor@gmail.com>
2021-12-15 16:32:12 -0500

    Update Evapotranspiration.R

--- Evapotranspiration.R
+++ Evapotranspiration.R
@@ -1,78 +1,14 @@
-library("plyr")
-library("dplyr")
-library("rgee")
-library("tidyverse")
-library('abind')
-library("sf")
-library("geojsonio")
+library('ggplot2')
+library('reshape2')
 library('tidyr')
-library("lubridate")
 library('plotly')
-ee_Initialize()
+library("aTSA")
+library("xts")
+library("lubridate")
+library("abind")
+library("ForeCA")
 
-
-###*** IMPORT EVAPOTRANSPIRATION TIME SERIES
-
-# import watersheds
-waterS <- ee$FeatureCollection("USGS/WBD/2017/HUC06")
-
-# extract watershed codes (level 3: Basin)
-waterSheds <- waterS$aggregate_array("huc6")$getInfo()
-
-ET.Data <- list()
-
-# loop to grab Evapotranspiration (ET) time series and discard watersheds outside the CONUS
-row <- 0
-for (i in waterSheds) {
-  
-  row <- row + 1
-  print(row)
-  
-  # select one watershed by iteration
-  mask <- ee$FeatureCollection("USGS/WBD/2017/HUC06")$filter(ee$Filter$eq("huc6", i))
-  # convert to geometry and extract the boundaries
-  region <- mask$geometry()$bounds()
-  
-  # load the NLCD data set and build a forest mask
-  nlcd <- ee$ImageCollection("USGS/NLCD")$
-    filterDate("2016-01-01", "2016-12-31")$
-    filterBounds(mask)$
-    first()$
-    select("landcover")$
-    eq(list(41,42,43))$ 
-    reduce(ee$Reducer$sum())
-  
-  # load ET imageCollection in forest pixels
-  ET <- ee$ImageCollection("CAS/IGSNRR/PML/V2")$
-    filterDate("2003-01-01", "2017-12-31")$
-    filterBounds(mask)$
-    map(function(x) x$reproject("EPSG:4326", NULL, 500)$select("Ec","Es","Ei")$reduce(ee$Reducer$sum()))$
-  # mask non-forest
-    map(function(x) x$updateMask(nlcd))
-
-  # extract ET values and dates by watershed (spatial summation of all the pixel values within the watershed)
-  ee_nc_ET <- try(ee_extract(x = ET$select("sum"), y = region, sf = FALSE, scale = 500))
-  if(class(ee_nc_ET) %in% 'try-error') {next} else {
-    
-    colnames(ee_nc_ET) <- substring(colnames(ee_nc_ET), regexpr("X2", colnames(ee_nc_ET)) + 1)
-    ee_nc_ET$name <- "ET"
-    
-    # convert into time series
-    time <- as.Date(colnames(ee_nc_ET[1, -ncol(ee_nc_ET)]), format = "%Y.%m.%d")
-    if (ncol(ee_nc_ET)==1) {next} else {
-      data <- t(ee_nc_ET[1, -ncol(ee_nc_ET)])
-      ET.Data[[row]] <- data.frame(ET = data[, 1], Date = time, row.names = NULL)
-      names(ET.Data)[[row]] <- i
-      
-      # clean memory
-      rm(list=setdiff(ls(), c("ET.Data", "i", "waterSheds", "row")))
-      gc(full = TRUE)
-    }
-  }
-}
-
-ET.Data <- compact(ET.Data)
-
+load("~/")
 
 ### convert the list of watersheds into a data frame
 annualET <- matrix(NA, nrow = length(lubridate::year(ET.Data[[1]][, 2])), ncol = length(ET.Data))
